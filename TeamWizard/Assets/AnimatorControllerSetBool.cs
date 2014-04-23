7925812ea2a7e3c158e55c9f1da8e8bb954abb6a
tyrawr <ty.sundon@gmail.com>
2014-04-23 17:24:41 -0600

    adding bool controller script

---
+++ TeamWizard/Assets/AnimatorControllerSetBool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorControllerSetBool : MonoBehaviour 
+{
+	public string boolName = "";
+	public bool boolValue = false;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Animator>().SetBool(boolName /* "Talking" */, boolValue /* true or false */ );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
