1134c0f702dee8a3c1b929cc66c4c10e15c95cd5
tyrawr <ty.sundon@gmail.com>
2014-04-23 20:18:50 -0600

    adding PRO image effects and animation stuff

---
+++ TeamWizard/Assets/RunOnKeyPress.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunOnKeyPress : MonoBehaviour {
+
+	public KeyCode keyID = KeyCode.C;
+	private bool active = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( Input.GetKeyDown(keyID) )
+		{
+			active = !active;
+			this.GetComponent<Animator>().SetBool("Running" , active );
+		}
+	}
+}
---
+++ TeamWizard/Assets/TalkOnKeyPress.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkOnKeyPress : MonoBehaviour {
+	public KeyCode keyID = KeyCode.Z;
+	private bool active = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( Input.GetKeyDown(keyID) )
+		{
+			active = !active;
+			this.GetComponent<Animator>().SetBool("Talking" , active );
+		}
+	}
+}
