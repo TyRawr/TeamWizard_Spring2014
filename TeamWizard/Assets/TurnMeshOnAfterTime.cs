0373334176d9c8592ae779f1b3c6203fe715b5f2
tyrawr <ty.sundon@gmail.com>
2014-04-27 11:18:36 -0600

    adding some control scripts

---
+++ TeamWizard/Assets/TurnMeshOnAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnMeshOnAfterTime : MonoBehaviour {
+	float time = 0f;
+	public float threshold = 1f;
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > threshold)
+			GetComponent<MeshRenderer>().enabled = true;
+	}
+}
